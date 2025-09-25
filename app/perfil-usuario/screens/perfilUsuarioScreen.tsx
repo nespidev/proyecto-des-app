9586639736ef799a5e1d0ef6fc28cf0b6528ca46
LucasCoquet <Lucasscoquet@gmail.com>
2025-09-25 17:54:14 -0300

    mockup perfilUsuario

--- app/perfil-usuario/screens/perfilUsuarioScreen.tsx
+++ app/perfil-usuario/screens/perfilUsuarioScreen.tsx
@@ -1,26 +1,54 @@
 import React from "react";
-import { View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "@/app/navigation/RootNavigation";
-import Button from "@/components/Button";
 import { globalStyles } from "@/utils/globalStyles";
-import { ScrollView } from "react-native-gesture-handler";
 
 type Props = NativeStackScreenProps<RootStackParamList, "PerfilUsuario">;
 
-export default function ChatScreen({ navigation }: Props) {
+const usuarioPrueba = {
+  nombre: "Lucas Coquet",
+  edad: 21,
+  peso: "80kg",
+  objetivo: "fuerza",
+  correo: "lucasscoquet@gmail.com",
+  tel: "123456789"
+};
+
+export default function PerfilUsuarioScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
       <Text style={globalStyles.title}>Perfil</Text>
       <Text style={globalStyles.subtitle}>Pantalla de Perfil</Text>
-      <Button
-        title="Detalles Perfil"
-        onPress={() => console.log("Detalles Perfil presionado")}
-      />
+
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Text style={styles.info}>{usuarioPrueba.nombre}</Text>
+        <Image source={require("../../../assets/user-predetermiando.png")} style={{ width: 120, height: 120, borderRadius: 60 }}  />
+        <Text style={styles.info}>Edad: {usuarioPrueba.edad}</Text>
+        <Text style={styles.info}>Corre: {usuarioPrueba.correo}</Text>
+        <Text style={styles.info}>Peso: {usuarioPrueba.peso}</Text>
+        <Text style={styles.info}>Tel: {usuarioPrueba.tel}</Text>
+        <Text style={styles.info}>Objetivo: {usuarioPrueba.objetivo}</Text>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center", padding: 20  },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "center", // centra vertical
+    alignItems: "center",     // centra horizontal
+    padding: 20,
+  },
+  info: {
+    fontSize: 16,
+    marginBottom: 8,
+    textAlign: "center",
+  },
+});
+
