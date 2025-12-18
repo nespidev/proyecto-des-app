19b94a7e17684137a1b988aed0547e9ab463cda1
Nahuel Espinola G <nahuelespinolagrativol@gmail.com>
2025-12-18 09:04:05 -0300

    movido editor de planes a carpeta components

--- app/tabs/screens/clients/clients-stack.tsx
+++ app/tabs/screens/clients/clients-stack.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ClientsListScreen from './clients-list';
 import ClientDashboardScreen from './client-dashboard';
-import PlanEditorScreen from './plan-editor';
+import PlanEditorScreen from '@/components/PlanEditorScreen';
 import PlanHistoryScreen from '../plan-history';
 import { materialColors } from '@/utils/colors';
 
